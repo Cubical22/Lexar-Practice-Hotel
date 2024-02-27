5574e2c4af5a2eeebd1fd7dea135344d19d193de
Amir Hossain <tailheadeusage@gmail.com>
2024-02-27 19:28:26 +0330

    the first commit with the bases

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require("http").Server(app);
+const data = require("./routes/data");
+
+// routes: 1. the display 2. data
+
+// 2.
+app.use("/data", data);
+
+// 1.
+app.get("/", (req,res)=>{
+    res.render("main.ejs");
+});
+
+http.listen(3000, ()=>{
+    console.log("server started on port 3000 successfuly <3\nhttp://localhost:3000/");
+});

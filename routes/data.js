32ecfcfe76d2007ce575a0cbb8b8918d06df681e
Amir Hossain <tailheadeusage@gmail.com>
2024-03-01 11:21:20 +0330

    made the filtered selection link

--- routes/data.js
+++ routes/data.js
@@ -3,8 +3,66 @@ const client = require("./../postgre/client");
 
 client.connect();
 
-router.get("/", (req,res)=>{
-    res.send("this is a data sending bit")
+router.get("/conditioned-apartment", async (req,res)=>{
+    // the plan is simple
+    // using req.query, we get a dictionary of all queries passed onto the selection
+    // based on that query set, we all create a request string that will be used inside the postgres client
+    // and the output will be passed directly into the user as json
+
+    let request_string = "SELECT * FROM apartments";
+    let is_first_condition = true;
+
+    const check_condition = (condition_name, is_number = false, comparison_sign = "=", query_name = undefined) => {
+        if (query_name === undefined) query_name = condition_name;
+        if (req.query[condition_name]) {
+            let value = undefined;
+            if (is_number) {
+                value = String(req.query[condition_name]);
+            } else {
+                value = `'${req.query[condition_name]}'`
+            }
+
+            request_string = request_string.concat(` ${is_first_condition ? "WHERE" : "AND"} ${query_name} ${comparison_sign} ${value}`);
+            is_first_condition = false;
+        }
+    }
+
+    if (req.query["id"]) { // having the id means not needing anything else
+        request_string = request_string.concat(` hotel_id='${req.query["id"]}'`);
+        res.send((await client.query(request_string)).rows);
+        console.log(request_string);
+        return;
+    }
+
+    check_condition("title", false, "=");
+
+    check_condition("min-rate", true, ">", "rate");
+    check_condition("rate", true, "=",);
+
+    check_condition("min-guests", true, ">", "guests");
+    check_condition("guests", true, "=");
+
+    check_condition("min-bedroom", true, ">", "bedroom");
+    check_condition("bedroom", true, "=");
+
+    check_condition("min-bathroom", true, ">", "bathroom");
+    check_condition("bathroom", true, "=");
+
+    check_condition("type", true, "=");
+
+    check_condition("geo", false, "=");
+
+    check_condition("location", false, "=");
+    
+    check_condition("free_cancellation", true, "=");
+
+    check_condition("instant_book", true, "=");
+
+    check_condition("min-price", true, ">", "price");
+    check_condition("max-price", true, "<", "price");
+    check_condition("price", true, "=");
+
+    res.send((await client.query(request_string)).rows);
 });
 
 module.exports = router;

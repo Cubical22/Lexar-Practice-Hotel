95599c7a30c1d7d1d18d98bbe07587c09f49675d
Amir Hossain <tailheadeusage@gmail.com>
2024-04-10 23:55:03 +0330

    added some extra comments to the database related file

--- postgre/client.js
+++ postgre/client.js
@@ -1,5 +1,9 @@
 const {Client} = require('pg');
 
+// you have to set up your own server using the admin
+// then change the fields below relating to your own data base
+// just make sure you have entered everything correctly
+// and used the backup data
 const client = new Client({
     user: "postgres",
     port: 5432,

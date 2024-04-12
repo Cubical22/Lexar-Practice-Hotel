c09b3c854dedab8111d9d36d9b56a650d301bdd3
Amir Hossain <tailheadeusage@gmail.com>
2024-04-12 13:41:24 +0330

    using a .env file to hold the password

--- postgre/client.js
+++ postgre/client.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const {Client} = require('pg');
 
 // you have to set up your own server using the admin
 // then change the fields below relating to your own data base
 // just make sure you have entered everything correctly
 // and used the backup data
+console.log(process.env);
+
 const client = new Client({
     user: "postgres",
     port: 5432,
-    password: "AmirHosane2",
+    password: process.env["PG_PASS"],
     database: "Hotel"
 });
 
